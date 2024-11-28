571dc674c4c7827f5b459dd95764f522aaf7ce54
Juan Paz <juan.paz.h@gmail.com>
2024-11-28 04:23:27 +0000

    agentai

--- src/chat.py
+++ src/chat.py
@@ -18,32 +18,6 @@ genai.configure(api_key=os.environ.get("GOOGLE_API_KEY"))
 def init_qamodel():
     return RAGModel()
 
-class QAApp:
-    def __init__(self):
-        self.qamodel = init_qamodel()
-        
-    def generate_response(self):
-        request = st.session_state.request
-        response = self.qamodel(request=request)
-
-        st.session_state.history.append({"message": request,"is_user":True})
-        st.session_state.history.append({"message": response['answer'],"is_user":False})
-
-        st.session_state.something = st.session_state.request
-        st.session_state.input_text = ""
-
-    def run_app(self):
-        st.title("Agente Servicio al Cliente")
-        if "history" not in st.session_state:
-            st.session_state.history = []
-        if st.button("Limpiar Chat"):
-            st.session_state.history = []
-             
-        st.text_input("Escribe tu mensaje", key="request", on_change=self.generate_response)
-        #st_message.chat_input("input_text", placeholder="Escribe tu mensaje")
-
-        for i,chat in enumerate(st.session_state.history):
-            st.message(**chat,key=str(i))
 
 if __name__ == "__main__":
     app = QAApp()
